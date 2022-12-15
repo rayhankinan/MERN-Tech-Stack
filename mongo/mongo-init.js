411b9d0b86af2a960ab94707bc5d661bcb964d3d
rayhankinan <rayhankinan@gmail.com>
2022-12-15 15:29:53 +0700

    Fix kibana

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,10 +1,10 @@
 db.createUser({
-    user: "rayhankinan",
-    pwd: "password",
+    user: process.env.MONGO_INITDB_USERNAME,
+    pwd: process.env.MONGO_INITDB_PASSWORD,
     roles: [
         {
             role: "readWrite",
-            db: "mern",
+            db: process.env.MONGO_INITDB_DATABASE,
         },
     ],
 });
